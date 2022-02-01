75c565a52e4a2653a8f15e61168182b04b7e28ca
Diego Gualdrón <dgualdron04@gmail.com>
2022-02-01 10:48:14 -0500

    Primer commit

---
+++ src/modules/daybook/router/index.js
@@ -0,0 +1,19 @@
+
+export default {
+    
+    name: 'daybook',
+    component: () => import(/* webpackChunkName: "Daybook" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
+    children: [
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "Daybook-no-entry" */ '@/modules/daybook/views/NoEntrySelected.vue'),
+        },
+        {
+            path: ':id',
+            name: 'entry',
+            component: () => import(/* webpackChunkName: "Daybook-entry" */ '@/modules/daybook/views/EntryView.vue'),
+        }
+    ]
+
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Nisi duis qui eu officia non sit mollit non qui ipsum laborum aliqua. Occaecat qui do ad deserunt sint commodo. Consectetur consectetur Lorem proident ipsum est fugiat exercitation mollit ex ipsum aliquip minim eiusmod sunt. Nulla deserunt ut adipisicing laboris dolore proident culpa irure cupidatat eu laborum qui.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Et eiusmod in nostrud culpa cupidatat veniam pariatur consectetur. Anim eu laborum elit ipsum voluptate officia. Lorem proident quis sunt pariatur elit exercitation commodo Lorem officia anim laborum.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Laboris laborum laboris et irure eiusmod Lorem aute et in id sint aliqua. Eu adipisicing fugiat do sunt. Laboris sunt et officia occaecat dolor sit consectetur ut dolor ullamco commodo fugiat aliqua. Commodo consectetur ut anim quis minim non eu nulla laborum. Occaecat pariatur minim labore mollit cillum sit aliqua. Nulla deserunt sit exercitation elit dolore aliqua esse. Labore labore id culpa nulla sit cupidatat et voluptate in labore enim proident et eu.',
+            picture: null, // https://
+        },
+    ]
+})
